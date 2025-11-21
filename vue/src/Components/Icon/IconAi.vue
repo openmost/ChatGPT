4b0841ee87b81c70e1b115845eb0f25664751da5
Ronan HELLO <ronan@mbp-de-ronan.home>
2025-11-21 13:03:14 +0100

    update: MistralAI LeChat logo

--- vue/src/Components/Icon/IconAi.vue
+++ vue/src/Components/Icon/IconAi.vue
@@ -17,55 +17,18 @@
   </svg>
   <!-- eslint-enable max-len -->
 
-  <svg v-if="aiName === 'mistral-ai'" width="256px" height="233px" viewBox="0 0 256 233"
-       version="1.1"
-       xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid">
-    <g>
-      <rect fill="#000000" x="186.181818" y="0" width="46.5454545" height="46.5454545"></rect>
-      <rect fill="#F7D046" x="209.454545" y="0" width="46.5454545" height="46.5454545"></rect>
-      <rect fill="#000000" x="0" y="0" width="46.5454545" height="46.5454545"></rect>
-      <rect fill="#000000" x="0" y="46.5454545" width="46.5454545" height="46.5454545"></rect>
-      <rect fill="#000000" x="0" y="93.0909091" width="46.5454545" height="46.5454545"></rect>
-      <rect fill="#000000" x="0" y="139.636364" width="46.5454545" height="46.5454545"></rect>
-      <rect fill="#000000" x="0" y="186.181818" width="46.5454545" height="46.5454545"></rect>
-      <rect fill="#F7D046" x="23.2727273" y="0" width="46.5454545" height="46.5454545"></rect>
-      <rect fill="#F2A73B" x="209.454545" y="46.5454545" width="46.5454545" height="46.5454545">
-
-      </rect>
-      <rect fill="#F2A73B" x="23.2727273" y="46.5454545" width="46.5454545"
-            height="46.5454545"></rect>
-      <rect fill="#000000" x="139.636364" y="46.5454545" width="46.5454545"
-            height="46.5454545"></rect>
-      <rect fill="#F2A73B" x="162.909091" y="46.5454545" width="46.5454545"
-            height="46.5454545"></rect>
-      <rect fill="#F2A73B" x="69.8181818" y="46.5454545" width="46.5454545"
-            height="46.5454545"></rect>
-      <rect fill="#EE792F" x="116.363636" y="93.0909091" width="46.5454545"
-            height="46.5454545"></rect>
-      <rect fill="#EE792F" x="162.909091" y="93.0909091" width="46.5454545"
-            height="46.5454545"></rect>
-      <rect fill="#EE792F" x="69.8181818" y="93.0909091" width="46.5454545"
-            height="46.5454545"></rect>
-      <rect fill="#000000" x="93.0909091" y="139.636364" width="46.5454545"
-            height="46.5454545"></rect>
-      <rect fill="#EB5829" x="116.363636" y="139.636364" width="46.5454545"
-            height="46.5454545"></rect>
-      <rect fill="#EE792F" x="209.454545" y="93.0909091" width="46.5454545"
-            height="46.5454545"></rect>
-      <rect fill="#EE792F" x="23.2727273" y="93.0909091" width="46.5454545"
-            height="46.5454545"></rect>
-      <rect fill="#000000" x="186.181818" y="139.636364" width="46.5454545"
-            height="46.5454545"></rect>
-      <rect fill="#EB5829" x="209.454545" y="139.636364" width="46.5454545"
-            height="46.5454545"></rect>
-      <rect fill="#000000" x="186.181818" y="186.181818" width="46.5454545"
-            height="46.5454545"></rect>
-      <rect fill="#EB5829" x="23.2727273" y="139.636364" width="46.5454545"
-            height="46.5454545"></rect>
-      <rect fill="#EA3326" x="209.454545" y="186.181818" width="46.5454545"
-            height="46.5454545"></rect>
-      <rect fill="#EA3326" x="23.2727273" y="186.181818" width="46.5454545"
-            height="46.5454545"></rect>
+  <svg v-if="aiName === 'mistral-ai'" width="365" height="258" viewBox="0 0 365 258" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <g id="Mistral AI Logo">
+      <path d="M104.107 0H52.0525V51.57H104.107V0Z" fill="#FFD800"/>
+      <path d="M312.351 0H260.296V51.57H312.351V0Z" fill="#FFD800"/>
+      <path d="M156.161 51.5701H52.0525V103.14H156.161V51.5701Z" fill="#FFAF00"/>
+      <path d="M312.353 51.5701H208.244V103.14H312.353V51.5701Z" fill="#FFAF00"/>
+      <path d="M312.356 103.14H52.0525V154.71H312.356V103.14Z" fill="#FF8205"/>
+      <path d="M104.107 154.71H52.0525V206.28H104.107V154.71Z" fill="#FA500F"/>
+      <path d="M208.228 154.711H156.174V206.281H208.228V154.711Z" fill="#FA500F"/>
+      <path d="M312.351 154.711H260.296V206.281H312.351V154.711Z" fill="#FA500F"/>
+      <path d="M156.195 206.312H0V257.882H156.195V206.312Z" fill="#E10500"/>
+      <path d="M364.439 206.312H208.244V257.882H364.439V206.312Z" fill="#E10500"/>
     </g>
   </svg>
 </template>
